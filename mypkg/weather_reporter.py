a6a4adb9c4cabe0e3ffd7dc373a443ff1358886f
Yuma Fukuya <fuku_yuma@outlook.com>
2025-01-02 01:29:38 +0900

    improve

--- mypkg/weather_reporter.py
+++ mypkg/weather_reporter.py
@@ -1,18 +1,17 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-import random  # ランダム選択のために追加
 
 class WeatherReporter(Node):
     def __init__(self):
         super().__init__('weather_reporter')
         self.publisher_ = self.create_publisher(String, 'weather_advice_topic', 10)
         self.timer = self.create_timer(2.0, self.publish_weather_advice)
-        self.weather_conditions = ["晴れ", "雨", "曇り", "雪", "強風"]  # 天気の選択肢
+        self.weather_conditions = ["晴れ", "雨", "曇り", "雪", "強風"]
+        self.index = 0
 
     def publish_weather_advice(self):
-        # ランダムに天気を選ぶ
-        weather = random.choice(self.weather_conditions)
+        weather = self.weather_conditions[self.index]
         advice_msg = String()
 
         if weather == "晴れ":
@@ -26,9 +25,11 @@ class WeatherReporter(Node):
         elif weather == "強風":
             advice_msg.data = "強風が吹いています。外出には注意してください！"
 
-        # ログに出力し、メッセージをトピックにパブリッシュ
         self.get_logger().info(f'Publishing: "{advice_msg.data}"')
         self.publisher_.publish(advice_msg)
+
+        # 次の天気に移行
+        self.index = (self.index + 1) % len(self.weather_conditions)
 
 def main(args=None):
     rclpy.init(args=args)
